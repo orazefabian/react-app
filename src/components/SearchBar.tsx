3f395f816699a4992585b44b29b311026fab835d
Fabian <oraze.fabian@icloud.com>
2023-10-02 10:44:44 +0200

    changed look of search bar + addition of keyup event on search input

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import search_icon from "../assets/search.png";
+import search_icon from "../assets/search.jpeg";
 import "../App.css";
 interface SearchBarProps {
   onSearch: (city: string) => void;
@@ -15,24 +15,35 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
     onSearch(query);
   };
 
+  const handleEnterSearch = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      onSearch(query);
+    }
+  };
+
   return (
-    <div className="logo container row mt-5 justify-content-center ">
-      <div>
-        <img src={search_icon} className="col-sm img-fluid"></img>
+    <div className="container-fluid mt-5">
+      <div className="row">
+        <img
+          src={search_icon}
+          className="col-sm img-fluid"
+          style={{ maxHeight: "4em", maxWidth: "4em" }}
+        ></img>
         <input
-          className="col-sm form-control rounded border-0"
+          className="rounded form-control border-0 col-sm"
+          style={{ fontSize: "14pt" }}
           type="text"
           placeholder="City..."
           value={query}
           onChange={handleInputChange}
+          onKeyUp={handleEnterSearch}
         />
       </div>
-      <button
-        className="col-sm input-group-append btn btn-primary rounded"
-        onClick={handleSearch}
-      >
-        Search
-      </button>
+      <div className="row">
+        <button className="btn btn-primary rounded" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
     </div>
   );
 };
